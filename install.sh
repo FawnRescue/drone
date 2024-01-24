a8b1d7c5418763234d214d832bf99765f65542fb
Lars Benedikt Kaesberg <l.kaesberg@stud.uni-goettingen.de>
2024-01-24 13:59:49 +0100

    feat: refactored deploy script

--- install.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-## This startup script runs ON the remote machine
-JAR_NAME=drone-1.0-SNAPSHOT.jar
-pkill -f 'java -jar /home/pi/helloworld/${JAR_NAME}'
-nohup java -jar ~/helloworld/${JAR_NAME} > ~/helloworld/$(date -d "today" +"%Y%m%d%H%M").log
-exit
--- start.sh
+++
@@ -1,8 +0,0 @@
-./gradlew clean build
-
-JAR_NAME=drone-1.0-SNAPSHOT.jar
-ssh drone@openhd 'mkdir -p ~/helloworld'
-
-scp -r ./build/libs/${JAR_NAME} drone@openhd:~/helloworld/
-
-cat install.sh | ssh drone@openhd 'bash -s'
